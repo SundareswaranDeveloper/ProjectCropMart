3595eb3649cabee160417112be58b5af32d47ff0
SundareswaranDeveloper <msundareswaran4@gmail.com>
2025-01-22 00:00:15 +0530

    Customer My Order page & Customer Orders for farmer page backend process related code updated

--- Pro1/src/org/newserver/FarmContol.java
+++ Pro1/src/org/newserver/FarmContol.java
@@ -1,6 +1,7 @@
 package org.newserver;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -38,8 +39,20 @@ public class FarmContol extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//doGet(request, response);
-		int a = Integer.parseInt(request.getParameter("avalue"));
-		String id = request.getParameter("secret");
+		int a = 0;
+		int b = 0;
+		try {
+			a = Integer.parseInt(request.getParameter("avalue"));
+		} catch (Exception e) {
+			a = (int)request.getAttribute("clue");
+			b++;
+		}
+		String id = (String) request.getParameter("secret");
+		if(b > 0) {
+			id = (String) request.getAttribute("cropmart");
+		}
+		System.out.println(a + "<-1--");
+		System.out.println("--1->" + id);
 		if (a==1) {
 			request.setAttribute("crtid",id);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("SellProduct.jsp");
@@ -67,6 +80,10 @@ public class FarmContol extends HttpServlet {
 		}else if(a==7) {
 			request.setAttribute("crtid",id);
 			RequestDispatcher dispatcher = request.getRequestDispatcher("CustomerMyCart");
+		    dispatcher.forward(request, response);		
+		}else if(a==8) {
+			request.setAttribute("crtid",id);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("FarmerOrderServlet");
 		    dispatcher.forward(request, response);		
 		}else {
 			response.sendRedirect("Index.jsp");
