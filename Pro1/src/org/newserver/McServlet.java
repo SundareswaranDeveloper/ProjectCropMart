9b84a9117b50ebc81a024455601b77c86606fb85
SundareswaranDeveloper <msundareswaran4@gmail.com>
2025-01-25 09:21:31 +0530

    FIX: Farmer page backend code updated!

--- Pro1/src/org/newserver/McServlet.java
+++ Pro1/src/org/newserver/McServlet.java
@@ -12,76 +12,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class McServlet
- */
 @WebServlet("/McServlet")
 public class McServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public McServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
-		System.out.println("welcome");
-		String tip = "Loading..Data...";
-		Database d = new Database();
-		List<Object> listMc = new ArrayList<Object>();
+
+		String[][] info = new String[4][];
+		Database db = new Database();
 		String id = (String)request.getAttribute("crtid");
-		request.setAttribute("crtid",id);
-		request.setAttribute("data1",tip);
-		request.setAttribute("data2",tip);
-		request.setAttribute("data3",tip);
-		request.setAttribute("data4",tip);
-		request.setAttribute("data5",tip);
-		
 		try {
-			listMc = d.readMc();
+			info = db.readCorner();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Integer c = (Integer) listMc.get(0);
-		request.setAttribute("count",c);
-		if (c>=1) {
-			request.setAttribute("data1",listMc.get(1));
-			if (c>=2) {
-				request.setAttribute("data2",listMc.get(2));
-				if (c>=3) {
-					request.setAttribute("data3",listMc.get(3));
-					if (c>=4) {
-						request.setAttribute("data4",listMc.get(4));
-						if (c>=5) {
-							request.setAttribute("data5",listMc.get(5));
-
-						}
-					}
-				}
-			}
+		request.setAttribute("crtid",id);
+		request.setAttribute("count",info[0][0]);
+		request.setAttribute("data-info",info[1]);
+		request.setAttribute("date-info",info[2]);
+		request.setAttribute("time-info",info[3]);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("Farmer2.jsp");
 	    dispatcher.forward(request, response);
-		}else {
-			System.out.println("ERROR");
-		}
 		
+		}
 	}
-
-}
--- Pro1/src/org/newserver/TipsServlet.java
+++ Pro1/src/org/newserver/TipsServlet.java
@@ -18,93 +18,28 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TipsServlet")
 public class TipsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TipsServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		/*System.out.println("welcome");
-		Database d = new Database();
-		String id = (String)request.getAttribute("crtid");
-		String tip = "Loading...";
-		try {
-			tip = d.readTips();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("tips is " + tip);
-		request.setAttribute("crtid",id);
-		RequestDispatcher dispatcher = request.getRequestDispatcher("Tips.jsp");
-	    dispatcher.forward(request, response);*/
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
-		/*Database d = new Database();
-		String tip = "Loading...";
-		try {
-			tip = d.readTips();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		response.sendRedirect("Tips.jsp?paramName= " + tip);
-		*/
-		System.out.println("welcome");
-		String tip = "Loading...";
-		Database d = new Database();
-		List<Object> list = new ArrayList<Object>();
-		String id = (String)request.getAttribute("crtid");
-		request.setAttribute("crtid",id);
-		request.setAttribute("tip1",tip);
-		request.setAttribute("tip2",tip);
-		request.setAttribute("tip3",tip);
-		request.setAttribute("tip4",tip);
-		request.setAttribute("tip5",tip);
 		
+		System.out.println("welcome");
+		String[][] tips = new String[4][];
+		Database db = new Database();
+		String id = (String)request.getAttribute("crtid");
 		try {
-			list = d.readTips();
+			tips = db.readTips();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Integer c = (Integer) list.get(0);
-		request.setAttribute("count",c);
-		if (c>=1) {
-			request.setAttribute("tip1",list.get(1));
-			if (c>=2) {
-				request.setAttribute("tip2",list.get(2));
-				if (c>=3) {
-					request.setAttribute("tip3",list.get(3));
-					if (c>=4) {
-						request.setAttribute("tip4",list.get(4));
-						if (c>=5) {
-							request.setAttribute("tip5",list.get(5));
-
-						}
-					}
-				}
-			}
+		request.setAttribute("crtid",id);
+		request.setAttribute("count",tips[0][0]);
+		request.setAttribute("data-tip",tips[1]);
+		request.setAttribute("date-tip",tips[2]);
+		request.setAttribute("time-tip",tips[3]);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("Tips.jsp");
 	    dispatcher.forward(request, response);
-		}else {
-			System.out.println("ERROR");
-		}
 		
 		}
+	
+	
 	}
